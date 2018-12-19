8f201ed648432093a17177f23297690dbd7a4181
pabloarancibia.dw@gmail.com <pabloarancibia.dw@gmail.com>
2018-12-19 13:48:46 -0300

    201812191348-todo a filessocket

--- pri113/socket/filessocket/Cliente.c
+++ pri113/socket/filessocket/Cliente.c
@@ -14,8 +14,8 @@
 #include <Socket.h>
 
 /* Programa principal.
- * Abre un socket, envia un numero al servidor y lee la respuesta de este, que 
- * es el mismo número incrementado en 1.
+ * Abre un socket, envia un numero al servidor y lee la respuesta de este, que
+ * es el mismo nï¿½mero incrementado en 1.
  */
 main ()
 {
@@ -25,12 +25,12 @@ main ()
 
    /* Para las llamadas a las funciones de sockets */
    int leidos;                   /* bytes leidos */
-   struct sockaddr_in Servidor;  /* Dirección del servidor */
-   socklen_t Longitud_Servidor;  /* Tamaño de la estructura Servidor */
+   struct sockaddr_in Servidor;  /* Direcciï¿½n del servidor */
+   socklen_t Longitud_Servidor;  /* Tamaï¿½o de la estructura Servidor */
 
    /*
     * Se abre un socket UDP para que el cliente pueda enviar y recibir mensajes
-    * a través de él. La función no lleva parámetros porque dejamos que unix se
+    * a travï¿½s de ï¿½l. La funciï¿½n no lleva parï¿½metros porque dejamos que unix se
     * encargue de los detalles.
    */
    Socket_Con_Servidor = Abre_Conexion_Udp ();
@@ -44,21 +44,21 @@ main ()
     * El servidor se le supone corriendo en la misma maquina (localhost) y
     * atendiendo al servicio cpp_java */
    Longitud_Servidor = sizeof(Servidor);
-   if (Dame_Direccion_Udp ("localhost", "cpp_java", &Servidor, 
+   if (Dame_Direccion_Udp ("localhost", "cpp_java", &Servidor,
       &Longitud_Servidor) == -1)
    {
       printf ("No se pueden obtener los datos del servidor\n");
       exit (-1);
    }
 
-   /* Se pone un número aleatorio en Datos, se muestra dicho numero por
+   /* Se pone un nï¿½mero aleatorio en Datos, se muestra dicho numero por
     * pantalla y se envia al servidor */
-   srand (time(NULL)); /* Semilla para números aleatorios */
+   srand (time(NULL)); /* Semilla para nï¿½meros aleatorios */
    Datos = rand()%20;  /* Aleatorio entre 0 y 19 */
    printf ("Envio %d\n", Datos);
 
-   /* Se envía el mensaje al servidor */
-   if (Escribe_Socket_Udp (Socket_Con_Servidor, (struct sockaddr *)&Servidor, 
+   /* Se envï¿½a el mensaje al servidor */
+   if (Escribe_Socket_Udp (Socket_Con_Servidor, (struct sockaddr *)&Servidor,
       Longitud_Servidor, (char *)&Datos, sizeof(Datos)) == -1)
    {
       printf ("Error al enviar datos al servidor\n");
@@ -66,10 +66,10 @@ main ()
    };
 
    /* Se leen los datos que nos envie el servidor como respuesta.
-    * El contenido de Servidor es indiferente, nos vendrá relleno con los
-    * datos del que transmite el mensaje. */ 
-   leidos = Lee_Socket_Udp (Socket_Con_Servidor, 
-         (struct sockaddr *)&Servidor, &Longitud_Servidor, 
+    * El contenido de Servidor es indiferente, nos vendrï¿½ relleno con los
+    * datos del que transmite el mensaje. */
+   leidos = Lee_Socket_Udp (Socket_Con_Servidor,
+         (struct sockaddr *)&Servidor, &Longitud_Servidor,
          (char *)&Datos, sizeof(Datos));
 
    /* Se escribe en pantalla los datos recibidos o el error si lo ha habido */
